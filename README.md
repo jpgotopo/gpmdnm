32f513204787f380f20df31077ae9a3e83fbfd8c
Jpgotopo <jpgotopo@GMAIL.COM>
2021-01-07 19:13:23 -0500

    modificaciones pequeñas

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# gpm_dnm
+# GPM Tracking
 
-A new Flutter project.
+Projecto de Aplicación de Seguimiento
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is a starting point for a Flutter application created for DNM
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+### Created by [JP Designs & Web](https://jpdesignsweb.com)
 
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,47 +1,15 @@
-import 'package:firebaseAuth/regster.dart';
-import 'package:firebaseAuth/signIn.dart';
 import 'package:flutter/material.dart';
-//https://github.com/jpgotopo/gpmdnm.git
-class RegisterPage extends StatefulWidget {
-  @override
-  _RegisterPageState createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Auth"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            child: OutlineButton(
-              child: Text("Signin"),
-              onPressed: () => _pushPage(context, SignIn()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          Container(
-            child: OutlineButton(
-              child: Text("Register"),
-              onPressed: () => _pushPage(context, Register()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
-}
+ 
+ class Register extends StatefulWidget {
+   @override
+   _RegisterState createState() => _RegisterState();
+ }
+ 
+ class _RegisterState extends State<Register> {
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       
+     );
+   }
+ }
